c7b02ab5b36d11ebe258aa249309266d65218ca0
flipmz <zorigmr@gmail.com>
2020-04-15 23:59:35 +0800

    setup forendpoint

---
+++ todo_project/urls.py
@@ -0,0 +1,7 @@
+# todo_project/urls.py
+from django.contrib import admin
+from django.urls import include, path  # new
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('todos.urls')),  # new
+]
